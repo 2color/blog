f1b922492d67aa582881586d5b67adcacf0d59c2
Daniel Norman <daniel@2color.me>
2021-10-08 12:31:24 +0200

    Fix double trailing slash in sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -192,7 +192,7 @@ module.exports = {
         output: '/sitemap.xml',
         serialize: ({ site, allSitePage }) =>
           allSitePage.edges.map(edge => ({
-            url: site.siteMetadata.siteUrl + edge.node.path + '/',
+            url: site.siteMetadata.siteUrl + edge.node.path,
             changefreq: 'daily',
             priority: 0.7
           }))
